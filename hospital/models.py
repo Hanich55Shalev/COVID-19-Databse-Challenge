f43b6dc88726e0c177270ba06e5a4a9550422672
Tranquillius <kotshalev@gmail.com>
2026-03-09 12:14:45 +0200

    Wrote HospitalWorker model

--- hospital/models.py
+++ hospital/models.py
@@ -13,3 +13,25 @@ class Department(models.Model):
 
     def __str__(self):
         return f'{self.hospital.name} - {self.name}'
+    
+class HospitalWorker(models.Model):
+    GENDER_CHOICES = (
+        ('M', "Male"),
+        ('F', 'Female'),
+        ('O', 'Other')
+    )
+
+    POSITION_CHOICES = (
+        ('doctor', 'Doctor'),
+        ('nurse', 'Nurse')
+    )
+
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    positon = models.CharField(max_length=10, choices=POSITION_CHOICES)
+    departments = models.ManyToManyField(Department)
+
+    def __str__(self):
+        return f'{self.positon} {self.name}'
+    
