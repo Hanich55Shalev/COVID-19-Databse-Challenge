b15ce0167a32e3baa49b4e096b11e26041ba7047
Tranquillius <kotshalev@gmail.com>
2026-03-09 12:44:25 +0200

    Added models to admin page to allow checking success

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Hospital, Department, HospitalWorker, Patient, MedicalExaminationResult
 
-# Register your models here.
+admin.site.register(Hospital)
+admin.site.register(Department)
+admin.site.register(HospitalWorker)
+admin.site.register(Patient)
+admin.site.register(MedicalExaminationResult)
